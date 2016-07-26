1603917b3421ad7eed931c9ad4f5db8c6dea088b
VKoorits <koorits.viktor@yandex.ru>
2016-07-26 15:50:06 +0300

    Update decision.h

--- decision.h
+++ decision.h
@@ -1,159 +1,112 @@
-#include"methods_decision.h"
+#include<cstdlib>
+#include<ctime>
+#include<conio.h>
 
-//решение
-void podbor(int *sudoku, int *variants, int *count_zero, long *count_resheniy, int *counter, int *level, bool boool);//прототип для reshenie()
-void sposobi(int *sudoku, int *variants, int *count_zero, long *count_resheniy, int *counter, int *level, bool boool){
-    char a,b,c,d,w='t';
-    do{
-        //одиночки
-        a=w;
-        w=search_clear_numbers(sudoku, variants, count_zero);
-        //cout << "clear " << w << endl;
-        if(w=='e')
-            return;
-        else if(w=='t'){
-            *(counter)+=1;
-            continue;
-        }
+#include "decision.h"
+#include "generator.h"
 
-        //скрытые одиночки
-        b=w;
-        w=search_hidden_clear_numbers(sudoku, variants, count_zero);
-        //cout << "hidden_clear ******* " << w << endl;
-        if(w=='e')
-            return;
-        else if(w=='t'){
-            *(counter+1)+=1;
-            continue;
-        }
 
-        //поиск по областям
-        c=w;
-        w=search_candidate_in_square(sudoku, variants);
-        //cout << "candidate_in_square ************** " << w << endl;
-        if(w=='e')
-            return;
-        else if(w=='t'){
-            *(counter+2)+=1;
-            continue;
+int main(){
+setlocale(0, "rus");
+    srand(time(NULL));
+    rand();
+    int sudoku[9][9], count_zero;
+    int level_sudoku(5);
+    char menu;
+
+    do{//СЃС‚Р°СЂС‚РѕРІРѕРµ РјРµРЅСЋ
+        cout << "\t\t*****РЎРЈР”РћРљРЈ-РњРђРЎРўР•Р ******\n\nРІР°СЃ РїСЂРёРІРµС‚СЃРІСѓРµС‚ РјР°СЃС‚РµСЂ-СЃСѓРґРѕРєСѓ. Р’С‹Р±РµСЂРёС‚Рµ СЂРµР¶РёРј СЂР°Р±РѕС‚С‹:" << endl;
+        cout << "1. Р“РµРЅРµСЂР°С†РёСЏ СЃСѓРґРѕРєСѓ" << endl;
+        cout << "2. Р РµС€РµРЅРёРµ СЃСѓРґРѕРєСѓ"  << endl;
+        cin >> menu;
+        system("cls");
+        if(!cin >> menu || (menu!='1' && menu!='2')){
+            cin.clear();
+            while(cin.get()!='\n');
         }else
             break;
+    }while(1);
 
-        //cкрытые пары
-        d=w;
-        w=test_on_hidden_pair(sudoku, variants);
-        //cout << "hidden_pair  ************************************ " << w << endl;
-        if(w=='e')
-            return;
-        else if(w=='t'){
-            *(counter+3)+=1;
-            continue;
-        }
+    if(menu=='1'){//РіРµРЅРµСЂР°С†РёСЏ
+        menu='0';
+        do{//level
+            cout << "\t\t*****РЎРЈР”РћРљРЈ-РњРђРЎРўР•Р ******\n\nР“РµРЅРµСЂР°С†РёСЏ СЃСѓРґРѕРєСѓ\nР’С‹Р±РµСЂРёС‚Рµ СѓСЂРѕРІРµРЅСЊ СЃР»РѕР¶РЅРѕСЃС‚Рё (РѕС‚ 1 РґРѕ 5)" << endl;
+            cout << "РЎР»РѕР¶РЅРѕСЃС‚СЊ СЃСѓРґРѕРєСѓ:\t";
+            cin >> menu;
+            system("cls");
+            if(!cin >> menu || int(menu)>53 || int(menu)<49){
+                cin.clear();
+                while(cin.get()!='\n');
+            }else
+                break;
+        }while(1);
 
-    }while((a=='t' || b=='t' || c=='t' || d=='t') && *count_zero!=0);
+        generator_sudoku(&sudoku[0][0], count_zero, int(menu)-48);
+        cout << "\t\t*****РЎРЈР”РћРљРЈ-РњРђРЎРўР•Р ******\n\n";
+        cout << "СЃРіРµРЅРµСЂРёСЂРѕРІР°РЅРЅРѕРµ СЃСѓРґРѕРєСѓ " << int(menu)-48 << " СѓСЂРѕРІРЅСЏ СЃР»РѕР¶РЅРѕСЃС‚Рё " << endl;
+        write_sudoku(&sudoku[0][0]);
+        cout << "\nС‡С‚РѕР±С‹ РїРѕСЃРјРѕС‚СЂРµС‚СЊ СЂРµС€РµРЅРёРµ СЃСѓРґРѕРєСѓ РЅР°Р¶РјРёС‚Рµ Р»СЋР±СѓСЋ РєРЅРѕРїРєСѓ";
 
-    if(*count_zero==0){
-        *count_resheniy+=1;
-        if(*count_resheniy==1){
-            if(boool){
-                write_sudoku(sudoku);
+        _getch();
+        system("cls");
 
+        cout << "\t\t*****РЎРЈР”РћРљРЈ-РњРђРЎРўР•Р ******\n\n";
+        cout << "СЃРіРµРЅРµСЂРёСЂРѕРІР°РЅРЅРѕРµ СЃСѓРґРѕРєСѓ " << int(menu)-48 << " СѓСЂРѕРІРЅСЏ СЃР»РѕР¶РЅРѕСЃС‚Рё " << endl;
+        write_sudoku(&sudoku[0][0]);
+        cout << "\nРµРґРёРЅСЃС‚РІРµРЅРЅРѕРµ СЂРµС€РµРЅРёРµ СЌС‚РѕРіРѕ СЃСѓРґРѕРєСѓ";
+        reshenie(&sudoku[0][0], 1);
+
+    }
+    else if(menu=='2'){
+        menu='0';
+        system("cls");
+        do{
+            cout << "\t\t*****РЎРЈР”РћРљРЈ-РњРђРЎРўР•Р ******\nР РµС€РµРЅРёРµ СЃСѓРґРѕРєСѓ\n\nРћС‚РєРґР° РїРѕР»СѓС‡РёС‚СЊ СЃСѓРґРѕРєСѓ:\n";
+            cout << "1. РР· С„Р°Р№Р»Р°" << endl;
+            cout << "2. РЎ РєР»Р°РІРёР°С‚СѓСЂС‹"  << endl;
+            cin >> menu;
+            system("cls");
+            if(!cin >> menu || (menu!='1' && menu!='2')){
+                cin.clear();
+                while(cin.get()!='\n');
+            }else
+                break;
+        }while(1);
+        system("cls");
+        if(menu=='1'){
+            ifstream fin;
+
+            do{
+            cout << "\t\t*****РЎРЈР”РћРљРЈ-РњРђРЎРўР•Р ******\nР РµС€РµРЅРёРµ СЃСѓРґРѕРєСѓ\n\nР’РІРµРґРёС‚Рµ Р°РґСЂРµСЃ С„Р°Р№Р»Р°:\t";
+            char filename[255];
+            cin.getline(filename, 255);
+            fin.open(filename);
+            system("cls");
+            if(fin.is_open()){
+                read_sudoku(&sudoku[0][0], fin, 'f');
+
+                cout << "\t\t*****РЎРЈР”РћРљРЈ-РњРђРЎРўР•Р ******\nР РµС€РµРЅРёРµ СЃСѓРґРѕРєСѓ\n\nРЎСѓРґРѕРєСѓ РёР· С„Р°Р№Р»Р°";
+                write_sudoku(&sudoku[0][0]);
+                reshenie(&sudoku[0][0], 1);
+                break;
             }
-            if(counter[5]<=51 && counter[0]<=7 && counter[1]+counter[2]+counter[3]+counter[4]==0)
-                *level=1;
-            else if(counter[0]<=10 && counter[1]+counter[2]+counter[3]+counter[4]==0)
-                *level=2;
-            else if(counter[2]+counter[3]<=3 && counter[4]==0)
-                *level=3;
-            else if(counter[4]<3)
-                *level=4;
-            else
-                *level=5;
-        }
-        return;
-    }else{
-        *(counter+4)+=1;
-        //cout << "podbor podbor podbor podbor ******************************" << endl;
-        podbor(sudoku, variants, count_zero, count_resheniy, counter, level, boool);
-    }
-}
-void podbor(int *sudoku, int *variants, int *count_zero, long *count_resheniy, int *counter, int *level, bool boool){
-    if(*count_resheniy>1)
-        return;
-    int min_variants=10;
-    int x,y;
-        //поиск места для вставки с наименьшим количеством вариантов
-        for(int i=0; i<9; i++)
-            for(int j=0; j<9; j++)
-                if(*(sudoku+i*9+j)==0 && *(variants+90*i+10*j)<min_variants){
-                    min_variants=*(variants+90*i+10*j);*(variants+90*i+10*j);
-                    x=i;
-                    y=j;
-                }
 
 
-        for(int num_variant=1; num_variant<=min_variants; num_variant++){
-            //создание судоку, которое можно изменять этой подфункции
-            int temp_sudoku[9][9];
-            for(int i=0; i<9; i++)
-                for(int j=0; j<9; j++)
-                    temp_sudoku[i][j]=*(sudoku+i*9+j);
+            }while(1);
+        }else if(menu=='2'){
+            cout << "\t\t*****РЎРЈР”РћРљРЈ-РњРђРЎРўР•Р ******\nР РµС€РµРЅРёРµ СЃСѓРґРѕРєСѓ\n\nР’РІРѕРґ СЃ РєР»Р°РІРёР°С‚СѓСЂС‹ (Р’РІРѕРґРёС‚Рµ СЃС‚СЂРѕРєРё СЃСѓРґРѕРєСѓ Р±РµР· РїСЂРѕР±РµР»РѕРІ)" << endl;
+            read_sudoku(&sudoku[0][0], cin, 'c');
+            system("cls");
+            cout << "\t\t*****РЎРЈР”РћРљРЈ-РњРђРЎРўР•Р ******\nР РµС€РµРЅРёРµ СЃСѓРґРѕРєСѓ\n\nР’РІРµРґРµРЅРЅРѕРµ СЃСѓРґРѕРєСѓ";
+            write_sudoku(&sudoku[0][0]);
+            int level;
+            level=reshenie(&sudoku[0][0], 1);
+            if(level<6)
+                cout << "Р”Р°РЅРЅРѕРµ СЃСѓРґРѕРєСѓ РёРјРµРµС‚ " << level << " СѓСЂРѕРІРµРЅСЊ СЃР»РѕР¶РіРѕСЃС‚Рё" << endl;
 
-            int temp_variants[9][9][10];
-            for(int i=0; i<9; i++)
-                for(int j=0; j<9; j++)
-                    for(int k=0; k<10; k++)
-                        temp_variants[i][j][k]=*(variants+i*90+j*10+k);
-
-            int lev[6]={*(counter),*(counter+1),*(counter+2),*(counter+3),*(counter+4), *(counter+5)};
-
-            int temp_count_zero=*count_zero;
-            temp_sudoku[x][y]=*(variants+90*x+10*y+num_variant);
-            searched_number(&temp_sudoku[0][0], &temp_variants[0][0][0], x, y, *(variants+90*x+10*y+num_variant), &temp_count_zero);
-
-            //вызов рекурсии подбор вариантов
-
-            sposobi(&temp_sudoku[0][0], &temp_variants[0][0][0], &temp_count_zero, count_resheniy, &lev[0], level, boool);
         }
 
-}
-
-int reshenie(int *sudoku_vxod, bool boool){
-    int sudoku[9][9];
-    int level=-1;
-    for(int i=0; i<9; i++)
-        for(int j=0; j<9; j++)
-            sudoku[i][j]=*(sudoku_vxod+i*9+j);
-    int count_zero=0;
-    long count_resheniy=0;
-    int count_variants=9,line=-1,column=-1;
-
-    int variants[9][9][10];
-    //обнуление элементов массива вариантов
-    for(int i=0; i<9; i++)
-        for(int j=0; j<9; j++)
-            for(int k=0; k<10; k++)
-                variants[i][j][k]=0;
-
-    zapolnenie(&sudoku[0][0], &variants[0][0][0], &count_zero);
-    if(count_zero>64){
-        if(boool)
-            cout << "Судоку имеет меньше 17 подсказок. Невозможно решить единственным способом" << endl;
-        return 1000;
     }
-    int counter[6]={0,0,0,0,0, count_zero};
-    sposobi(&sudoku[0][0], &variants[0][0][0], &count_zero, &count_resheniy, &counter[0], &level,  boool);
 
-    if(count_resheniy==1){
-        if(boool)
-            cout << "Судоку имеет единственное решение" << endl;
-        return level;
-    }else{
-        if(boool)
-            if(count_resheniy==0)
-                cout << "Cудоку не имеет решний" << endl;
-            else
-                cout << "Судоку нельзя решить единственным способом" << endl;
-        return 1000;
-    }
+_getch();
 }
