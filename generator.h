53697d5f4a62bb583747e61618c160f61fc45c8e
VKoorits <koorits.viktor@yandex.ru>
2016-07-26 16:02:04 +0300

    Русификация

--- generator.h
+++ generator.h
@@ -1,155 +1,125 @@
-#include<iostream>
-#include<fstream>
-
-using namespace std;
-
-//служебные функции
-void delete_from_arr(int *this_variants, int num){
-    if(*(this_variants)!=0){
-        int i=1;
-        for(i; i<10; i++)
-            if(*(this_variants+i)==num){
-                *(this_variants+i)=0;
-                *(this_variants)=*(this_variants)-1;
-                if(i!=9)
-                    while(*(this_variants+i+1)!=0){
-                        *(this_variants+i)=*(this_variants+i+1);
-                        i++;
-                    }
-                *(this_variants+i)=0;
-                break;
-            }
-    }
+//перемешивание базовой сетки
+void transposing(int *sudoku){//столбцы становятся строками и наоборот
+    for(int i=8; i>0; i--)
+        for(int j=0; j<=i; j++)
+            swap(*(sudoku+i*9+j), *(sudoku+j*9+i));
 }
-void searched_number(int *sudoku, int *variants, int i, int j, int num, int *count_zero){
-    if(num==0)return;
-    *count_zero-=1;
-    for(int b=0; b<10; b++)
-        *(variants+90*i+10*j+b)=0;
-    //просмотр столбца
-    for(int k=0; k<9; k++)
-        if(*(sudoku+k*9+j)==0){
-            delete_from_arr(variants+k*90+j*10, num);
-        }
-    //просмотр строки
-    for(int k=0; k<9; k++)
-        if(*(sudoku+i*9+k)==0){
-            delete_from_arr(variants+i*90+k*10, num);
-        }
-    //определение квадрата 3*3 в котором находится рассматриваемая клетка(его верхнего левого угла)
-    int x=(i/3)*3;
-    int y=(j/3)*3;
-    //просмотр квадрата
-    for(int a=x; a<x+3; a++)
-        for(int b=y; b<y+3; b++)
-            if(*(sudoku+a*9+b)==0){
-                delete_from_arr(variants+a*90+b*10, num);
-            }
-}
-bool search_in_arr(int *this_variants, int num, int i, int incrementer){
+void swap_strings_small(int *sudoku){//Обмен двух строк в пределах одного района
+    int oblast, str1, str2;
+
+    oblast=rand()%3;
+    str1=oblast*3+rand()%3;
+    do{
+        str2=oblast*3+rand()%3;
+    }while(str1==str2);
+
     for(int j=0; j<9; j++)
-        if(*(this_variants+j*incrementer+i)==num)//найден элемент
-            return true;
-    return false;
-}
-
-//ввод/вывод информации
-void read_sudoku(int *sudoku, istream &fin, char file_or_cmd){
-    int counter=0;
-    int c;
-    while(counter!=81){
-        c=int(fin.get()-48);
-        if(int(c)>=0 && int(c)<=9){
-            *(sudoku+counter)=c;
-            counter++;
-        }
-        if(file_or_cmd=='f' && fin.eof())
-            break;
-    }
-}
-template <class T>
-void write_sudoku(const T *sudoku){
-    cout << "\n ----------------------\n";
-    for(int i=0; i<9; i++){
-        cout << "|";
-        for(int j=0; j<9; j++){
-            if(*(sudoku +i*9+j)==0)
-                cout << "- ";
-            else
-            cout << *(sudoku +i*9+j) << " ";
-            if(j%3==2)
-                cout << "| ";
-        }
-        cout << "\n";
-        if(i%3==2)
-            cout << " ----------------------\n";
-    }
-}
-void write_variants(const int *variants){
-for(int i=0; i<9; i++){
-    for(int j=0; j<9; j++){
-        cout << "i=" << i << " j=" << j << " " << *(variants+i*90+j*10) << "(";
-        if(*(variants+i*90+j*10)<5)
-        for(int k=1; k<10; k++){
-            if(*(variants+i*90+j*10+k)==0){
-                cout << ")\t";
-                break;
-            }
-            cout << *(variants+i*90+j*10+k);
-        }
-        else
-            cout << "       ";
-    }
-     cout << endl;
-}
-cout << endl << endl;
+        swap(*(sudoku+str1*9+j), *(sudoku+str2*9+j));
 
 }
-void zapolnenie(const int *sudoku, int *variants, int *count_zero){
-    char choose[9];
-    int counter;
-    int this_variants[9];
-    int x,y;//координаты верхнего левого угла
+void swap_columns_small(int *sudoku){//Обмен двух столбцов в пределах одного района
+    int oblast, col1, col2;
 
-    for(int i=0; i<9; i++){
-        for(int j=0; j<9; j++){
+    oblast=rand()%3;
+    col1=oblast*3+rand()%3;
+    do{
+        col2=oblast*3+rand()%3;
+    }while(col1==col2);
 
-            //рассматриваем по одной все клетки
-            if(*(sudoku+i*9+j)==0){
-                *count_zero+=1;
-                //заполняем массив истинами
-                for(int k=0; k<9; k++){
-                    choose[k]='o';
-                    this_variants[k]=0;
+    for(int i=0; i<9; i++)
+        swap(*(sudoku+i*9+col1), *(sudoku+i*9+col2));
+
+}
+void swap_strings_area(int *sudoku){//обмен больших строк
+    int str1, str2;
+
+    str1=rand()%3;
+    do{
+        str2=rand()%3;
+    }while(str1==str2);
+
+    for(int j=0; j<9; j++)
+        for(int k=0; k<3; k++)
+            swap(*(sudoku+(str1*3+k)*9+j), *(sudoku+(str2*3+k)*9+j));
+}
+void swap_columns_area(int *sudoku){//обмен больших столбцов
+    int col1, col2;
+
+    col1=rand()%3;
+    do{
+        col2=rand()%3;
+    }while(col1==col2);
+
+    for(int i=0; i<9; i++)
+        for(int k=0; k<3; k++)
+            swap(*(sudoku+i*9+(col1*3+k)), *(sudoku+i*9+(col2*3+k)));
+}
+
+void generator_sudoku(int *sudoku, int &count_zero, int level){
+    int x, y, temp, count_prosmotrov, this_level, temp_level;
+    bool prosmotr[81], work;
+
+    do{
+        //заполнение базовой сетки
+        for(int b=0; b<3; b++)// big string
+            for(int k=0; k<3; k++){//small string
+            x=(b+1)+k*3;
+            for(int j=0; j<9; j++){
+                if(j==0)
+                    *(sudoku+(b*3+k)*9+j)=x;
+                else{
+                    if(*(sudoku+(b*3+k)*9+j-1)==9)
+                        *(sudoku+(b*3+k)*9+j)=1;
+                    else
+                        *(sudoku+(b*3+k)*9+j)=*(sudoku+(b*3+k)*9+j-1)+1;
                 }
-                //обнуляем счетчик
-                counter=0;
-
-                //просмотр столбца
-                for(int k=0; k<9; k++)
-                    if((sudoku+k*9+j)!=0)
-                        choose[*(sudoku+k*9+j)-1]='s';
-                //просмотр строки
-                for(int k=0; k<9; k++)
-                    if((sudoku+i*9+k)!=0)
-                        choose[*(sudoku+i*9+k)-1]='l';
-                //определение квадрата 3*3 в котором находится рассматриваемая клетка(его верхнего левого угла)
-                x=(i/3)*3;
-                y=(j/3)*3;
-                //просмотр квадрата
-                for(int a=x; a<x+3; a++)
-                    for(int b=y; b<y+3; b++)
-                        if(*(sudoku+a*9+b)!=0)
-                            choose[*(sudoku+a*9+b)-1]='q';
-
-                //заполняем варианты
-                for(int k=0; k<9; k++)
-                    if(choose[k]=='o'){
-                        counter++;
-                        *(variants+i*90+j*10+counter)=k+1;
-                    }
-                *(variants+i*90+j*10)=counter;
             }
         }
+
+        count_prosmotrov=count_zero=0;
+        for(int b=0; b<81; b++)
+            prosmotr[b]=false;
+
+        for(int i=0; i<20; i++){//перемешивание
+        x=rand()%5;
+        switch(x){
+            case 0:
+                transposing(sudoku);
+            break;
+            case 1:
+                swap_strings_small(sudoku);
+            break;
+            case 2:
+                swap_columns_small(sudoku);
+            break;
+            case 3:
+                swap_strings_area(sudoku);
+            break;
+            case 4:
+                swap_columns_area(sudoku);
+            break;
+        }
     }
+        while(count_prosmotrov!=81){//вычеркивание клеточек
+            do{//поиск ненулевой клетки
+                x=rand()%9;
+                y=rand()%9;
+            }while(*(sudoku+x*9+y)==0 || prosmotr[x*9+y]==true);
+
+            count_prosmotrov++;
+            //cout << count_prosmotrov;
+            prosmotr[x*9+y]=true;
+            temp=*(sudoku+x*9+y);
+            *(sudoku+x*9+y)=0;
+            temp_level=reshenie(sudoku, 0);
+            if(temp_level>level){//если полученное судоку имеет более высокий уровень, то отмена
+                *(sudoku+x*9+y)=temp;
+            }else{
+                count_zero++;
+                this_level=temp_level;
+                work=true;
+            }
+        }
+
+    }while(this_level!=level);
 }
